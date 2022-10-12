41f99c6e440ffa2b10c4571e3afc3f6fedb0a648
nicholas <realenderflames@gmail.com>
2022-10-12 17:49:40 -0500

    - DeathsListener code cleanup/fixing old bad practice.

--- src/main/java/io/github/nichxlas98/uhcore/listeners/DeathsListener.java
+++ src/main/java/io/github/nichxlas98/uhcore/listeners/DeathsListener.java
@@ -20,48 +20,60 @@ public class DeathsListener implements Listener {
     }
 
     private static void playerDeathMessages(PlayerDeathEvent e, Player killed, Player killer, DamageCause playerDeathCause) {
-        if (playerDeathCause == DamageCause.BLOCK_EXPLOSION) {
-            if (ModelsClass.getChance(50)) {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " pulled a kamikaze.");
-            } else {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " somehow blew himself up.");
-            }
-        } else if (playerDeathCause == DamageCause.CONTACT) {
-            e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " pricked himself to death.");
-        } else if (playerDeathCause == DamageCause.DROWNING) {
-            if (ModelsClass.getChance(50)) {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " couldn't swim.");
-            } else {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " ran out of breath & drowned.");
-            }
-        } else if (playerDeathCause == DamageCause.ENTITY_ATTACK && e.getEntity().getKiller() != null) {
-            if (ModelsClass.getChance(50)) {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " was out-matched in battle by " + ChatColor.RED + killer.getName() + ".");
-            } else {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " couldn't keep up with " + ChatColor.RED + killer.getName() + ".");
-            }
-        } else if (playerDeathCause == DamageCause.ENTITY_EXPLOSION) {
-            e.setDeathMessage(ChatColor.RED + killed.getName() + ChatColor.YELLOW + " exploded.");
-        } else if (playerDeathCause == DamageCause.FALL) {
-            if (ModelsClass.getChance(50)) {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " fell and broke his legs.");
-            } else {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " pulled a Luke Bryan.");
-            }
-        } else if (playerDeathCause == DamageCause.FIRE) {
-            e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " played with fire.");
-        } else if (playerDeathCause == DamageCause.LAVA) {
-            e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " tried to swim in lava.");
-        } else if (playerDeathCause == DamageCause.PROJECTILE && e.getEntity().getKiller() != null) {
-            if (ModelsClass.getChance(50)) {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " was beamed by " + ChatColor.RED + killer.getName() + ".");
-            } else {
-                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " was sniped by " + ChatColor.RED + killer.getName() + ".");
-            }
-        } else if (playerDeathCause == DamageCause.FIRE_TICK) {
-            e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " was brutally burned to death.");
-        } else {
-            e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " died.");
+
+        switch (playerDeathCause) {
+            case BLOCK_EXPLOSION:
+                if (ModelsClass.getChance(50)) {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " pulled a kamikaze.");
+                } else {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " somehow blew himself up.");
+                }
+                break;
+            case CONTACT:
+                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " pricked himself to death.");
+                break;
+            case DROWNING:
+                if (ModelsClass.getChance(50)) {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " couldn't swim.");
+                } else {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " ran out of breath & drowned.");
+                }
+                break;
+            case ENTITY_ATTACK:
+                if (ModelsClass.getChance(50)) {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " was out-matched in battle by " + ChatColor.RED + killer.getName() + ".");
+                } else {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " couldn't keep up with " + ChatColor.RED + killer.getName() + ".");
+                }
+                break;
+            case ENTITY_EXPLOSION:
+                e.setDeathMessage(ChatColor.RED + killed.getName() + ChatColor.YELLOW + " exploded.");
+                break;
+            case FALL:
+                if (ModelsClass.getChance(50)) {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " fell and broke his legs.");
+                } else {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " pulled a Luke Bryan.");
+                }
+                break;
+            case FIRE:
+                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " played with fire.");
+                break;
+            case LAVA:
+                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " tried to swim in lava.");
+                break;
+            case PROJECTILE:
+                if (ModelsClass.getChance(50)) {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " was beamed by " + ChatColor.RED + killer.getName() + ".");
+                } else {
+                    e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " was sniped by " + ChatColor.RED + killer.getName() + ".");
+                }
+                break;
+            case FIRE_TICK:
+                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " was brutally burned to death.");
+                break;
+            default:
+                e.setDeathMessage(ChatColor.RED + "[*] " + killed.getName() + ChatColor.YELLOW + " died.");
         }
     }
 }
