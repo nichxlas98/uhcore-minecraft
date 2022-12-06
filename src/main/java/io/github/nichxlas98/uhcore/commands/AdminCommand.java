389cca01f871bb8f31270c87543cbfe1a6f2f6de
nichxlas98 <djflames50@gmail.com>
2022-12-06 16:15:29 -0500

    - Cleaned up AdminCommand.java imports

--- src/main/java/io/github/nichxlas98/uhcore/commands/AdminCommand.java
+++ src/main/java/io/github/nichxlas98/uhcore/commands/AdminCommand.java
@@ -11,8 +11,8 @@ import org.bukkit.entity.Player;
 
 import static io.github.nichxlas98.uhcore.models.MessageModels.*;
 import static io.github.nichxlas98.uhcore.models.ModelsClass.playerAdminLevel;
-import static io.github.nichxlas98.uhcore.utils.AdminlevelUtil.LOW_ADMIN_LEVEL;
-import static io.github.nichxlas98.uhcore.utils.AdminlevelUtil.MIN_ADMIN_LEVEL;
+import static io.github.nichxlas98.uhcore.utils.AdminUtil.LOW_ADMIN_LEVEL;
+import static io.github.nichxlas98.uhcore.utils.AdminUtil.MIN_ADMIN_LEVEL;
 import static io.github.nichxlas98.uhcore.utils.ServerManagerUtil.scoreboardName;
 import static org.bukkit.ChatColor.*;
 
