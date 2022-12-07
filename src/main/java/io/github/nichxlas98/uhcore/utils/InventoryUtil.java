efdb811a6b69eeec3bcb0c2d9db5bbbb752de642
nichxlas98 <djflames50@gmail.com>
2022-12-07 17:38:31 -0500

    - Added more intense/thorough saving system (saves all item specifics except enchanted books)

--- src/main/java/io/github/nichxlas98/uhcore/utils/InventoryUtil.java
+++ src/main/java/io/github/nichxlas98/uhcore/utils/InventoryUtil.java
@@ -11,13 +11,79 @@ import static io.github.nichxlas98.uhcore.utils.DatabaseUtil.*;
 
 public class InventoryUtil {
 
+    private static String reverseString(String string) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] words = string.split("_");
+
+        for (int j = words.length-1; j >= 0; j--) {
+            stringBuilder.append(words[j]).append('_');
+        }
+        return stringBuilder.toString();
+    }
+    public static void saveInventoryEC(Player player) {
+        ArrayList<String> item_names = new ArrayList<>();
+        item_names.add("helmet");
+        item_names.add("chestplate");
+        item_names.add("leggings");
+        item_names.add("boots");
+        item_names.add("sword");
+        item_names.add("axe");
+        item_names.add("pickaxe");
+        item_names.add("shovel");
+        item_names.add("rod");
+        item_names.add("bow");
+        item_names.add("apple");
+        item_names.add("item");
+
+        int index = 0;
+        ItemStack[] items = new ItemStack[27];
+        for (ItemStack i : player.getInventory().getContents()) {
+            if (i == null) continue;
+            String item = reverseString(i.getType().name().toLowerCase());
+            if (!(item_names.contains(item.split("_")[0]))) continue;
+            items[index] = i;
+            index++;
+        }
+
+        for (ItemStack i : player.getInventory().getArmorContents()) {
+            if (i == null) continue;
+            String item = reverseString(i.getType().name().toLowerCase());
+            if (!(item_names.contains(item.split("_")[0]))) continue;
+            items[index] = i;
+            index++;
+        }
+
+        player.getEnderChest().setContents(items);
+    }
+
+    public static void loadInventoryEC(Player player) {
+        player.getInventory().setContents(player.getEnderChest().getContents());
+        player.getEnderChest().clear();
+    }
+
+
     public static void saveInventory(Player player) {
         ItemStack[] inventoryItems = player.getInventory().getContents();
         List<String> itemsToSave = new ArrayList<>();
+        ArrayList<String> item_names = new ArrayList<>();
+        item_names.add("helmet");
+        item_names.add("chestplate");
+        item_names.add("leggings");
+        item_names.add("boots");
+        item_names.add("sword");
+        item_names.add("axe");
+        item_names.add("pickaxe");
+        item_names.add("shovel");
+        item_names.add("rod");
+        item_names.add("bow");
+        item_names.add("apple");
+        item_names.add("item");
 
-        for (ItemStack item : inventoryItems) {
-            if (item == null) continue;
-            itemsToSave.add(item.getType().name() + ";" + item.getAmount());
+        for (ItemStack i : inventoryItems) {
+            if (i == null) continue;
+            String item = reverseString(i.getType().name().toLowerCase());
+            if (item_names.contains(item.split("_")[0])) continue;
+            itemsToSave.add(i.getType().name() + ";" + i.getAmount());
         }
 
         configBackup.set(player.getUniqueId() + ".Items", itemsToSave);
